afee4a058c7a9d6959cb518f92fae302897f4cd1
bilalbarki <bilalbarki@gmail.com>
2016-07-12 22:25:34 +0000

    added missing database names

--- api/config.py
+++ api/config.py
@@ -1,24 +1,27 @@
 import os
 
 AIRBNB_ENV = os.environ.get('AIRBNB_ENV')
+environments = ["development", "production"]
+database_names = {environments[0]: "airbnb_dev", environments[1]: "airbnb_prod"}
 
 # check for errors in the environment variable AIRBNB_ENV
 if AIRBNB_ENV == None:
-    print "Please set AIRBNB_ENV environment variable to either production or development!"
+    print "Please set AIRBNB_ENV environment variable to either %s or %s!" % (environments[0], environments[1])
     quit()
-elif AIRBNB_ENV != "development" and AIRBNB_ENV != "production":
+elif AIRBNB_ENV != environments[0] and AIRBNB_ENV != environments[1]:
     print "AIRBNB_ENV environment variable has an unsupported value!"
     quit()
 
 # set appropriate variables depending on development or production
-if AIRBNB_ENV == "development":
+if AIRBNB_ENV == environments[0]:
     DEBUG = True
     HOST = "localhost"
-    PORT = "3333"
+    PORT = 3333
     DATABASE = {
         "host": "158.69.92.163", 
         "user": "airbnb_user_dev",
-        "port": "3306",
+        "database": database_names[environments[0]],
+        "port": 3306,
         "charset": "utf8",
         "password": os.environ.get('AIRBNB_DATABASE_PWD_DEV'),
     }
@@ -26,11 +29,12 @@ if AIRBNB_ENV == "development":
 else:
     DEBUG = False
     HOST = "0.0.0.0"
-    PORT = "3000"
+    PORT = 3000
     DATABASE = {
         "host": "158.69.92.163",
         "user": "airbnb_user_prod",
-        "port": "3306",
+        "database": database_names[environments[1]],
+        "port": 3306,
         "charset": "utf8",
         "password": os.environ.get('AIRBNB_DATABASE_PWD_PROD'),
     }
