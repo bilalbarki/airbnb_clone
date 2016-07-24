142965a4b03868606dbfed2fdaa4f33c0b5bf4ad
Electra Chong <electrachong@gmail.com>
2016-07-24 20:01:29 +0000

    Push preliminary tests on user creation

--- api/tests/user.py
+++ api/tests/user.py
@@ -28,8 +28,34 @@ class UserTestCase(unittest.TestCase):
     # test cases for data that might get sent for creating a User
     def test_create(self):
         response = self.create('Jon', 'Snow', 'jon@snow.com', 'toto1234')
-        print response.data
-        assert '"id": 1,' in response.data
+        #print response.data
+        #assert '"id": 1,' in response.data
+        new_user = User.get(User.id == 1)
+        self.assertEqual(new_user.first_name, 'Jon');
+        self.assertEqual(new_user.last_name, 'Snow');
+        self.assertEqual(new_user.email, 'jon@snow.com');
+        assert new_user.password is not None
+
+        # test cases of missing parameters, should raise error:
+        with self.assertRaises(TypeError):
+            response = self.create('Jon', 'Snow', 'jon@snow.com')
+        with self.assertRaises(TypeError):
+            response = self.create('Jon', 'Snow', 'toto1234')
+        with self.assertRaises(TypeError):
+            response = self.create('Jon', 'jon@snow.com', 'toto1234')
+        with self.assertRaises(TypeError):
+            response = self.create('Snow', 'jon@snow.com', 'toto1234')
+
+        response = self.create('Kate', 'Snow', 'jon@snow.com', 'abcd1234')
+        #print response.data
+        #with self.assertRaises(UserDoesNotExist):
+        #    new_user = User.get(User.id == 2)
+        self.assertEquals(response.status_code, 409)
+        json_response = json.loads(response.data)
+        self.assertEqual(json_response['code'], 10000)
+        self.assertEqual(json_response['msg'], "Email already exists")
+        
+
         #response = self.create('Jon', 'Snow', 'jon@snow.com')
         #response = self.create('Jon', 'Snow', 'toto1234')
         #response = self.create('Jon', 'jon@snow.com', 'toto1234')
